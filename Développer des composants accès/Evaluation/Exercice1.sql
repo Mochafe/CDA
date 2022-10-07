6f1ca00576f88504d1332544ca7d57534b9cbd7c
Florian Pasquet <florianpasquett@gmail.com>
2022-10-07 11:10:36 +0200

    Modif Eval

--- Développer des composants accès/Evaluation/Exercice1.sql
+++ Développer des composants accès/Evaluation/Exercice1.sql
@@ -18,10 +18,52 @@ JOIN products ON suppliers.SupplierID = products.SupplierID
 WHERE Country = 'France'
 GROUP BY CompanyName;
 
-
+#4
 SELECT CompanyName AS Client, COUNT(*) AS 'Nbre commandes'
 FROM customers 
 JOIN orders ON customers.CustomerID = orders.CustomerID
 WHERE Country = 'France'
 GROUP BY CompanyName
 HAVING COUNT(*) > 10;
+
+#5
+SELECT CompanyName AS Client, SUM(UnitPrice * Quantity), Country AS Pays
+FROM customers 
+JOIN orders ON customers.CustomerID = orders.CustomerID
+JOIN `order details` ON orders.OrderID = `order details`.OrderID
+GROUP BY CompanyName
+HAVING SUM(UnitPrice * Quantity) > 30000
+ORDER BY SUM(UnitPrice * Quantity) DESC
+;
+
+#6
+SELECT DISTINCT Country AS Pays
+FROM customers
+JOIN orders ON customers.CustomerID = orders.CustomerID
+JOIN `order details` ON orders.OrderID = `order details`.OrderID
+JOIN products ON `order details`.ProductID = products.ProductID
+WHERE SupplierID = (SELECT SupplierID FROM suppliers WHERE CompanyName = 'Exotic Liquids')
+ORDER BY Country ASC
+;
+
+#7
+SELECT SUM(UnitPrice * Quantity) AS 'Montant Ventes 97'
+FROM `order details`
+JOIN orders ON `order details`.OrderID = orders.OrderID
+WHERE YEAR(OrderDate) = 1997; 
+
+#8
+SELECT MONTH(OrderDate) AS 'Mois 97', SUM(UnitPrice * Quantity) AS 'Montant Ventes'
+FROM `order details`
+JOIN orders ON `order details`.OrderID = orders.OrderID
+WHERE YEAR(OrderDate) = 1997
+GROUP BY MONTH(OrderDate);
+
+#9
+SELECT MAX(OrderDate) AS 'Date de dernière commande'
+FROM customers
+JOIN orders On customers.CustomerID = orders.CustomerID
+WHERE CompanyName = 'Du monde entier';
+
+SELECT AVG(DATEDIFF(RequiredDate, ShippedDate))
+FROM orders;
